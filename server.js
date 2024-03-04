9baac5f9888717512de5770d701ca862a5a2d1fc
RobinElseman <robinc.elseman@gmail.com>
2024-03-04 11:11:32 +0100

    Added test.ejs

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.listen(3000, () => console.log('Server is running on port 3000'));
 
 // A sample route, replace this with your own routes
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('test.ejs')
 })
 
 // Middleware to handle not found errors - error 404
