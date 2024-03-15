9a618de6db1f80194f8332f61b5126faa1f9e854
RobinElseman <robinc.elseman@gmail.com>
2024-03-15 16:51:06 +0100

    Added backdrop image

--- server.js
+++ server.js
@@ -95,7 +95,10 @@ app.get('/movie/:name', (req, res) => {
     const overview = json.results[0].overview
     const imageURL = json.results[0].poster_path
     const posterSrc = `https://image.tmdb.org/t/p/w500${imageURL}`
-    res.render('shrek.ejs', {title, overview, posterSrc})
+    const backdropURL = json.results[0].backdrop_path
+    const backdropSrc = `https://image.tmdb.org/t/p/w500${backdropURL}`
+    console.log(json.results[0])
+    res.render('shrek.ejs', {title, overview, posterSrc, backdropSrc})
   })
   .catch(err => console.error('error:' + err));
 }
