d8cdc2994ba2f4896a6f915f143b40b3a51cda5e
Jacco12 <jacco.mols@hva.nl>
2024-03-18 14:55:13 +0100

    Swiper function

---
+++ static/js/filmdetail.js
@@ -0,0 +1,29 @@
+import Swiper from 'swiper'
+import { Navigation, Pagination } from 'swiper/modules'
+
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
+const swiper = new Swiper('.swiper',  {
+    modules: [Navigation, Pagination],
+
+    direction: 'horizontal',
+    loop: 'true',
+    speed: 300,
+    cssMode: true,
+
+    pagination: {
+        el:  '.swiper-pagination',
+    },
+    
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    }
+}) 
+
+
+
+
+
