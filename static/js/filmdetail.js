e8ca0db192960bd12ec5b621885bb60685f6d128
Jacco12 <jacco.mols@hva.nl>
2024-03-26 12:02:22 +0100

    Swiper finish

--- static/js/filmdetail.js
+++ static/js/filmdetail.js
@@ -1,31 +1,9 @@
-//  const swiper = new Swiper ('.swiper',  {
-
-
-//     direction: 'vertical',
-//     loop: 'true',
-
-//     pagination: {
-//         el:  '.swiper-pagination',
-//     },
-    
-//     navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev'
-//     },
-
-//     // scrollbar: {
-//     //     el: '.swiper-scrollbar',
-//     //   },
-// }) 
-
-
-
-
 
 const swiper = new Swiper('.filmdetail-swiper', {
 
     direction: 'horizontal',
     loop: true,
+    spaceBetween: 15,
     pagination: {
     el: '.swiper-pagination',
     },
@@ -36,9 +14,29 @@ const swiper = new Swiper('.filmdetail-swiper', {
     scrollbar: {
       el: '.swiper-scrollbar',
     },
+
   });
   
 
+// const dropDownButton = document.querySelector('.filmdetail-recepten-button')
+// const menu = document.querySelector('.menu')
+
+// button.addEventListener('click', () => {
+//     menu.classList.toggle('activated');
+
+// });
+
+const dropDownButtons = document.querySelector('.filmdetail-dropdown-button')
 
 
+const menuButtonsArray = Array.from(menuButtons);
+
+menuButtonsArray.forEach(button => {
+
+    button.addEventListener('click', () => {
+       closeAnotherButtons(button);       
+       button.classList.toggle('activated');       
+              
+    });
+}); 
 
