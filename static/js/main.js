7cdbe3d4b5705b307a231102fbee75e1aca13688
gylianto <dasiranmonadjat@gmail.com>
2024-04-07 23:37:47 +0200

    added comments to the js

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,27 @@
+// Selecteer alle elementen met de klasse 'gsap-slide'
 const slides = document.querySelectorAll('.gsap-slide');
+
+// Stel de huidige slide in op 0 (eerste slide)
 let currentSlide = 0;
+
+// Bepaal de breedte van een slide
 const slideWidth = slides[0].offsetWidth;
+
+// Bepaal het totale aantal slides
 const totalSlides = slides.length;
 
+// Functie om naar een specifieke slide te gaan
 function slideTo(index) {
+    // Gebruik de GSAP library om te animeren naar de gewenste slide
     gsap.to('.gsap-wrapper', {
+        // De horizontale scrollpositie wordt bepaald door de breedte van een slide te vermenigvuldigen met de index van de gewenste slide
         scrollLeft: slideWidth * index,
+        // De duur van de animatie is 1 seconde
         duration: 1,
+        // De animatie versnelt in het begin en vertraagt aan het einde
         ease: 'power1.inOut'
     });
+    // Update de huidige slide naar de index van de slide waar we naartoe geanimeerd hebben
     currentSlide = index;
 }
 
